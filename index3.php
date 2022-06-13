28c93468ba8b2110514cb4dc5b203cec4f651db0
Taras-Bilyk <bilyk.taras2007@gmail.com>
2022-06-13 17:26:05 +0300

    Change 5

--- index3.php
+++ index3.php
@@ -23,7 +23,7 @@
         array_push($answersDecode, $_POST["answerNumberOf5P"]);
 
         
-        var_dump($_POST);
+        // var_dump($_POST);
 
         // $answersDecode = json_decode($_SESSION["sAnswers"]);
 
