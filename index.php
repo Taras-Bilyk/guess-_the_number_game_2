68a6402940e512eb9cb2c80694e1c17a6bf1ffaa
Taras-Bilyk <bilyk.taras2007@gmail.com>
2022-06-13 14:59:50 +0300

    Change 2

--- index.php
+++ index.php
@@ -9,40 +9,40 @@
     <body>
         <form action="index2.php" method="POST">
             <?php
-        session_start();
-        // session_unset();
-        $sPoints = $_SESSION["sPoints"];
-        $number = rand(1, 100);
-        if($sPoints == ""){
-            $_SESSION["sPoints"] = 0;
-            echo("<h5>Всього балів: $sPoints</h5>");
-        }else{
-            echo("<h5>Всього балів: $sPoints</h5>");
-        }
-        $points = 10;
-        $b1 = 0;
-        $b2 = 0;
-        $b3 = 0;
-        echo("<p>$number</p>");
-        echo("<h4>Виберіть діапазон</h4>");
-        echo("<select name='answer'>");
-        $v = 1;
-        for($i = 1; $i <= 10; $i++){
-            $r1 = $i * 10;
-            $p = "$v-$r1";
-            echo("<option value=$p>$v-$r1</option>");
-            $v += 10;
-        }
-        echo("</select>");
+                session_start();
+                // session_unset();
+                $sPoints = $_SESSION["sPoints"];
+                $number = rand(1, 100);
+                if($sPoints == ""){
+                    $_SESSION["sPoints"] = 0;
+                    echo("<h5>Всього балів: $sPoints</h5>");
+                }else{
+                    echo("<h5>Всього балів: $sPoints</h5>");
+                }
+                $points = 10;
+                $b1 = 0;
+                $b2 = 0;
+                $b3 = 0;
+                // echo("<p>$number</p>");
+                echo("<h4>Виберіть діапазон</h4>");
+                echo("<select name='answer'>");
+                $v = 1;
+                for($i = 1; $i <= 10; $i++){
+                    $r1 = $i * 10;
+                    $p = "$v-$r1";
+                    echo("<option value=$p>$v-$r1</option>");
+                    $v += 10;
+                }
+                echo("</select>");
 
-    
-        echo("<input type='hidden' name='number' value='$number'>");
-        echo("<input type='hidden' name='points' value='$points'>");
-        echo("<input type='hidden' name='b1' value='$b1'>");
-        echo("<input type='hidden' name='b2' value='$b2'>");
-        echo("<input type='hidden' name='b3' value='$b3'>");
-        echo("<input type='submit' value='ok'>");
-    ?>
-    </form>
-</body>
+            
+                echo("<input type='hidden' name='number' value='$number'>");
+                echo("<input type='hidden' name='points' value='$points'>");
+                echo("<input type='hidden' name='b1' value='$b1'>");
+                echo("<input type='hidden' name='b2' value='$b2'>");
+                echo("<input type='hidden' name='b3' value='$b3'>");
+                echo("<input type='submit' value='ok'>");
+            ?>
+        </form>
+    </body>
 </html>
